6762240ffaf90099ae14b87281c9be6c6ebd6767
kkasyan <104075234+kkasyan@users.noreply.github.com>
2023-03-13 18:52:59 +0200

    deleted non-usable code

--- src/components/ContactForm/Input/Input.jsx
+++
@@ -1,3 +0,0 @@
-export const Input = ({ value, name }) => {
-  return <input name={name} value={value}></input>;
-};
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,12 +1,6 @@
 export const getContacts = ({ contacts }) => contacts;
 
 export const getFilteredContacts = ({ contacts = [], filter }) => {
-  // if (!contacts) {
-  //   return [];
-  // }
-  // if (!filter) {
-  //   return contacts;
-  // }
   const normalizedFilter = filter.toLowerCase();
   const result = contacts.filter(({ name }) => {
     const normalizedName = name.toLowerCase();
